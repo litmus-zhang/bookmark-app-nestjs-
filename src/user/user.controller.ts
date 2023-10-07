c18dbb49a238a7864e5f1b0067a14d34f3c5f342
Lukmon Abdulsalam <abdulsalamlukmon9@gmail.com>
2023-10-07 07:55:41 +0100

    added terraform for IaC

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { GetUser } from '../../src/auth/decorators';
-import { JwtGuard } from '../../src/auth/guard';
+import { GetUser } from '../auth/decorators';
+import { JwtGuard } from '../auth/guard';
 import { EditUserDto } from './dto';
 import { UserService } from './user.service';
 
